9984ed9394a58f5c6ba2939e8f8270f3a82185d3
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-12-10 17:55:44 -0600

    Add Nav Links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div className="nav-header">
+        <h3>Jacob Melancon | Portfolio</h3>
+      </div>
+      <div className="nav-links">
+        <a href="#">Home</a>
+        <a href="#">Skills</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
