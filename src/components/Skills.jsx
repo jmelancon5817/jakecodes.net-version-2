7f96119be7ebba853a470b94a0f98d48886752e9
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-12-13 15:37:09 -0600

    Update Skills Grid Layout

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,28 +4,28 @@ function Skills() {
   return (
     <div id="skills" className="skills-container">
       <div className="skills-item">
-        <h3 className="skills-title">HTML</h3>
+        <h2 className="skills-title">HTML</h2>
         <p className="skills-description">Hyper Text Markup Language</p>
       </div>
       <div className="skills-item">
-        <h3 className="skills-title">HTML</h3>
-        <p className="skills-description">Hyper Text Markup Language</p>
+        <h2 className="skills-title">CSS</h2>
+        <p className="skills-description">Cascading Style Sheets</p>
       </div>
       <div className="skills-item">
-        <h3 className="skills-title">HTML</h3>
-        <p className="skills-description">Hyper Text Markup Language</p>
+        <h2 className="skills-title">JavaScript</h2>
+        <p className="skills-description">Programming Language</p>
       </div>
       <div className="skills-item">
-        <h3 className="skills-title">HTML</h3>
-        <p className="skills-description">Hyper Text Markup Language</p>
+        <h2 className="skills-title">VS Code</h2>
+        <p className="skills-description">Code Editor</p>
       </div>
       <div className="skills-item">
-        <h3 className="skills-title">HTML</h3>
-        <p className="skills-description">Hyper Text Markup Language</p>
+        <h2 className="skills-title">React</h2>
+        <p className="skills-description">JavaScript Library</p>
       </div>
       <div className="skills-item">
-        <h3 className="skills-title">HTML</h3>
-        <p className="skills-description">Hyper Text Markup Language</p>
+        <h2 className="skills-title">Git</h2>
+        <p className="skills-description">Version Control System</p>
       </div>
     </div>
   );
