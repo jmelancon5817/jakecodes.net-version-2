ada6fb840d98d915e8cbc8180b9d547fe7015449
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-12-13 13:09:17 -0600

    Update Skills Section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 function Skills() {
-  return <div>Skills</div>;
+  return (
+    <div id="skills" className="skills-container">
+      <div className="skills-item">
+        <h3 className="skills-title">HTML</h3>
+        <p className="skills-description">Hyper Text Markup Language</p>
+      </div>
+      <div className="skills-item">
+        <h3 className="skills-title">HTML</h3>
+        <p className="skills-description">Hyper Text Markup Language</p>
+      </div>
+      <div className="skills-item">
+        <h3 className="skills-title">HTML</h3>
+        <p className="skills-description">Hyper Text Markup Language</p>
+      </div>
+      <div className="skills-item">
+        <h3 className="skills-title">HTML</h3>
+        <p className="skills-description">Hyper Text Markup Language</p>
+      </div>
+      <div className="skills-item">
+        <h3 className="skills-title">HTML</h3>
+        <p className="skills-description">Hyper Text Markup Language</p>
+      </div>
+      <div className="skills-item">
+        <h3 className="skills-title">HTML</h3>
+        <p className="skills-description">Hyper Text Markup Language</p>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
