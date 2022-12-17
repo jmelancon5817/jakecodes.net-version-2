629d592275dd2f030fbd67d84b2443ba1deb0691
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-12-16 18:38:41 -0600

    Add Contact Form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,53 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div id="contact" className="contact-container">
+      <div className="contact-text">
+        <h2 id="contact-headline">Contact</h2>
+        <p className="contact-description">Let's get to work</p>
+      </div>
+      <div className="contact-form">
+        <form
+          action="https://getform.io/f/be461f3c-6531-4524-bd62-991670990617"
+          method="POST"
+        >
+          <div>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Name"
+              required
+            />
+          </div>
+
+          <div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+
+          <div>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Message"
+              cols="30"
+              rows="10"
+              required
+            ></textarea>
+          </div>
+
+          <button className="underline">Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
