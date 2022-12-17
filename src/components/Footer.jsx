cf9796d2f3e5bfa2b17c75e196a389ae444370b4
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-12-17 11:36:39 -0600

    Add Footer Social Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
     <div className="footer-container">
       <div className="footer-text">Jacob Melancon | Portfolio</div>
-      <div className="footer-links">T G I</div>
+      <div className="footer-links">
+        <a href="https://github.com/jmelancon5817">
+          <FaGithub size={25} />
+        </a>
+        <a href="https://instagram.com/jake__melancon">
+          <FaInstagram size={25} />
+        </a>
+        <a href="https://twitter.com/melancon_jake">
+          <FaTwitter size={25} />
+        </a>
+      </div>
     </div>
   );
 }
