714e6435ce0f00cd1c6254d5d0c6d735832dbaa5
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-12-16 18:39:18 -0600

    Add Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-text">Jacob Melancon | Portfolio</div>
+      <div className="footer-links">T G I</div>
+    </div>
+  );
 }
 
 export default Footer;
